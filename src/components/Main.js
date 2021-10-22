5faff5d207da00ee26ba86bfa018ad114775135f
SquidsOnSol <93012597+SquidsOnSol@users.noreply.github.com>
2021-10-22 18:01:46 -0400

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,12 @@ export default function Main() {
             <div className="root-container">
                 <div className="content-section">
                     <h1>Squids on Sol</h1>
+         <div className="joining">
+                        <div className="social-icons">
+                            <a href="/"><img src={twitter_icon} alt="twitter" /></a>
+                            <a href="/"><img src={discord_icon} alt="discord" /></a>
+                        </div>
+                    </div>
         <div className="faqs">
                         <span>An collection of <b>2500 code generated</b> squids in from ASCII art.</span><br /><br />
         </div>
@@ -18,32 +24,9 @@ export default function Main() {
                     </div>
                     <div className="img-wrapper"></div>
         <div className="faqs">
-                        <h2>INFORMATION</h2><br /><br />
+                        <h2>FAQ</h2><br /><br />
                         <span> IN PROGRESS</span><br />
         </div>
-        <div className="joining">
-                        <div className="social-icons">
-                            <a href="/"><img src={twitter_icon} alt="twitter" /></a>
-                            <a href="/"><img src={discord_icon} alt="discord" /></a>
-                        </div>
-                    </div>
-                    <div className="faqs">
-                        <h2>FAQ</h2><br /><br />
-                        <span>WHAT IS THE LAUNCH DATE?</span>
-                        <p>2 PM UTC, Octuber 5th 2021</p><br />
-                        <span>HOW MANY SQUIDS ARE IN TOTAL?</span>
-                        <p>2500</p>
-                        <div className="faq-img">
-                            <div className="faq-content">
-                                <span>WHAT BLOCKCHAIN?</span>
-                                <p>SOLANA</p><br />
-                                <span>PRICE?</span>
-                                <p>0.1 SOL</p>
-                            </div>
-                        </div>
-                        <span>WHAT MARTKETPLACE WILL IT BE AVAILABLE IF I MISS MINT?</span>
-                        <p>Magic Eden & solanart </p>
-                    </div>
                     <div className="toadmap">
                         <h2>ROADMAP</h2><br /><br />
                         <p>IN PROGRESS</p> <br />
