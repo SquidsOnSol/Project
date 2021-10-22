a817237f148dde3a0f9c47e310be788c1a75a2f3
SquidsOnSol <93012597+SquidsOnSol@users.noreply.github.com>
2021-10-22 17:32:16 -0400

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,11 +10,17 @@ export default function Main() {
         <>
             <div className="root-container">
                 <div className="content-section">
-                    <h1>SolFrogs</h1>
+                    <h1>Squids on Sol</h1>
                     <div className="img-wrapper"></div>
                     <div className="button">
                         <button>Connect Wallet</button>
                     </div>
+        <div className="faqs">
+                        <h2>INFORMATION</h2><br /><br />
+                        <span> LAUNCH DATE?</span><br />
+<span> LAUNCH DATE?</span><br />
+<span> LAUNCH DATE?</span><br />
+        </div>
                     <div className="faqs">
                         <h2>FAQ</h2><br /><br />
                         <span> LAUNCH DATE?</span>
@@ -28,26 +34,14 @@ export default function Main() {
                                 <span>MINT PRICE?</span>
                                 <p>0.1 SOL</p>
                             </div>
-                            <img src={faq} alt="faq_img" />
                         </div>
                         <span>MARTKETPLACE?</span>
-                        <p>Magio Eden, solanart, </p>
-                        <p>Digital Eyes ASAP post mint</p>
+                        <p>Magio Eden & solanart </p>
                     </div>
                     <div className="toadmap">
-                        <h2>TOADMAP</h2><br /><br />
+                        <h2>ROAMAP</h2><br /><br />
                         <p>IN PROGRESS</p> <br />
-                        <div className="in-progress-list">
-                            <li>Complete the rescue of the 6969 Solforgs</li>
-                            <li>list on all available marketplace</li>
-                            <li>lauch new website and TOADAO</li>
-                        </div><br />
-                        <p>NEXT</p> <br />
-                        <div className="in-progress-list">
-                            <li>TOADrop NTFs to TOADZ holders</li>
-                            <li>Offer physical toad themed airdrop</li>
-                            <li>donate % of royalites to wildlife charity</li>
-                        </div>
+                        
                     </div>
                     <div className="joining">
                         <h2>join the swamp</h2>
